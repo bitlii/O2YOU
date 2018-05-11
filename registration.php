593161edd2409a1863e30ef8ceb968a637117197
bitlii <chenjcaki@gmail.com>
2018-05-11 15:45:51 +1200

    basic registration form
    first name, last name, email, password, confirm password and a sign up button.

---
+++ registration.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+	<title>O2YOU - Registration</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	
+</head>
+<body class="registration-container">
+<div class="form-container">
+	<form id="registration"	name="registration-form" method="post" action="" enctype="multipart/formdata">
+		<h1>Registration</h1>
+		<div class="form-element"><input name="first-name" type="text" required="required" class="text-field" placeholder="First Name"></div>
+		<div class="form-element"><input name="last-name" type="text" required="required" class="text-field" placeholder="Last Name"></div>
+		<div class="form-element"><input name="email" type="text" required="required" class="text-field" placeholder="Email"></div>
+		<div class="form-element"><input name="password" type="text" required="required" class="text-field" placeholder="Password"></div>
+		<div class="form-element"><input name="confirm-password" type="text" required="required" class="text-field" placeholder="Confirm Password"></div>
+		<div class="form-element"><input name="sign-up" type="submit" class="button" value="SIGN UP"></div>
+	</form>
+</div>
+</body>
+</html>
