9e41420ab74509c5f188418eb7cb5c4ac64332d1
bitlii <chenjcaki@gmail.com>
2018-05-11 15:23:37 +1200

    header and nav
    basic start

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+	<title>O2YOU - The number one marketplace for your oxygen needs</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+	<!-- META -->
+	<meta charset="utf-8"/>
+    <meta name="author" content="Jackie Chen"/>
+    <meta name="description" content="O2YOU's is the worlds biggest oxygen marketplace with a wide selection of oxygen ready to ship at moments notice."/>
+    <meta name="keywords" content="o2you, oxygen, marketplace, o2, air"/>
+</head>
+<body>
+	<!-- Header Area -->
+	<header>
+		<h1>O<sub>2</sub>YOU <span id="slogan">The only oxygen you will ever need</span></h1>
+	</header>
+
+	<!-- Navigation Bar -->
+	<nav>
+		<ul>
+			<li class="current"><a href="">HOME</a></li>
+			<li><a href="registration.php">REGISTER</a></li>
+			<li><a href="login.php">LOGIN</a></li>
+			<li class="cart"><a href="cart.php">CART</a></li>
+		</ul>
+	</nav>
+
+	
+	
+</body>
+</html>
